ad32ae54a85a35b213de9c79bd5694c03dad90db
ruihao.zhai@yooo.fun <ruihao.zhai@yooo.fun>
2020-05-15 16:40:48 +0800

    更换头像

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,94 +1,89 @@
 module.exports = {
-  "title": "sleep",
-  "description": "what's up",
-  "dest": "html",
-  "head": [
-    [
-      "link",
-      {
-        "rel": "icon",
-        "href": "/favicon.ico"
-      }
-    ],
-    [
-      "meta",
-      {
-        "name": "viewport",
-        "content": "width=device-width,initial-scale=1,user-scalable=no"
-      }
-    ]
-  ],
-  "theme": "reco",
-  "themeConfig": {
-    "nav": [
-      {
-        "text": "Home",
-        "link": "/",
-        "icon": "reco-home"
-      },
-      {
-        "text": "TimeLine",
-        "link": "/timeline/",
-        "icon": "reco-date"
-      },
-      {
-        "text": "Contact",
-        "icon": "reco-message",
-        "items": [
-          {
-            "text": "GitHub",
-            "link": "https://github.com/zhairuihao",
-            "icon": "reco-github"
-          }
+    "title": "sleep",
+    "description": "what's up",
+    "dest": "html",
+    "head": [
+        [
+            "link",
+            "link",
+            {
+                "rel": "icon",
+                "href": "/favicon.ico"
+            }
+        ],
+        [
+            "meta",
+            {
+                "name": "viewport",
+                "content": "width=device-width,initial-scale=1,user-scalable=no"
+            }
         ]
-      }
     ],
-    "type": "blog",
-    "blogConfig": {
-      "category": {
-        "location": 2,
-        "text": "Category"
-      },
-      "tag": {
-        "location": 3,
-        "text": "Tag"
-      }
-    },
-    "friendLink": [
-      {
-              "title": "github",
-              "desc": "github 主页地址",
-              "avatar": "https://avatars1.githubusercontent.com/u/18378213?s=460&u=c67240e3260765429f5a63e27d3d5be674edc2b4&v=4",
-              "link": "https://github.com/zhairuihao"
-      },
-      {
-        "title": "午后南杂",
-        "desc": "Enjoy when you can, and endure when you must.",
-        "email": "1156743527@qq.com",
-        "link": "https://www.recoluan.com"
-      },
-      {
-        "title": "vuepress-theme-reco",
-        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
-        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
-        "link": "https://vuepress-theme-reco.recoluan.com"
-      },
-    ],
-    "logo": "/logo.png",
-    "search": true,
-    "searchMaxSuggestions": 10,
-    "sidebar": "auto",
-    "lastUpdated": "Last Updated",
-    "author": "zhairuihao",
-    "authorAvatar": "https://avatars1.githubusercontent.com/u/18378213?s=460&u=c67240e3260765429f5a63e27d3d5be674edc2b4&v=4",
-    "record": "xxxx",
-    "startYear": "2020",
-     vssueConfig: {
-          platform: 'github',
-          owner: 'zhairuihao',
-          repo: 'vuepress-reco',
-          clientId: '4eeb9cd2f7249af6d11e',
-          clientSecret: 'abe80266714d4500634a99e7dcb7ca9feaf448c8',
+    "theme": "reco",
+    "themeConfig": {
+        "nav": [
+            {
+                "text": "Home",
+                "link": "/",
+                "icon": "reco-home"
+            },
+            {
+                "text": "TimeLine",
+                "link": "/timeline/",
+                "icon": "reco-date"
+            },
+            {
+                "text": "Contact",
+                "icon": "reco-message",
+                "items": [
+                    {
+                        "text": "GitHub",
+                        "link": "https://github.com/zhairuihao",
+                        "icon": "reco-github"
+                    }
+                ]
+            }
+        ],
+        "type": "blog",
+        "blogConfig": {
+            "category": {
+                "location": 2,
+                "text": "Category"
+            },
+            "tag": {
+                "location": 3,
+                "text": "Tag"
+            }
+        },
+        "friendLink": [
+            {
+                "title": "午后南杂",
+                "desc": "Enjoy when you can, and endure when you must.",
+                "email": "1156743527@qq.com",
+                "link": "https://www.recoluan.com"
+            },
+            {
+                "title": "vuepress-theme-reco",
+                "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+                "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+                "link": "https://vuepress-theme-reco.recoluan.com"
+            },
+        ],
+        "logo": "/logo.png",
+        "search": true,
+        "searchMaxSuggestions": 10,
+        "sidebar": "auto",
+        "lastUpdated": "Last Updated",
+        "author": "zhairuihao",
+        "authorAvatar": "/avatar.jpg",
+        "record": "1.2.3.4",
+        "startYear": "2020",
+        vssueConfig: {
+            platform: 'github',
+            owner: 'zhairuihao',
+            repo: 'vuepress-reco',
+            clientId: '4eeb9cd2f7249af6d11e',
+            clientSecret: 'abe80266714d4500634a99e7dcb7ca9feaf448c8',
         },
         // 密钥
 //        keyPage: {
@@ -96,8 +91,8 @@ module.exports = {
 //          color: '#42b983', // 登录页动画球的颜色
 //          lineColor: '#42b983', // 登录页动画线的颜色
 //        },
-  },
-  "markdown": {
-    "lineNumbers": true
-  }
+    },
+    "markdown": {
+        "lineNumbers": true
+    }
 }
